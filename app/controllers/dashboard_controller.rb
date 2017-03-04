e8a05e7b8625db19356082d32d59ec1f4b24e9f6
kishore bjv <jb619d@att.com>
2017-03-05 02:12:45 +0530

    Adding prerefence_update form to dashboard index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,11 @@ class DashboardController < ApplicationController
   end
 
   def preference_update
+   current_user.likes=params.select{|key,value| value == "like"}.keys
+   current_user.dislikes=params.select{|key,value| value == "dislike"}.keys
+   current_user.no_pref=params.select{|key,value| value == "no_pref"}.keys
+   current_user.save
+    
+   redirect_to :back
   end
 end
