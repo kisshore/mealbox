bc4078cdbacf326931a485390e0612167de61459
kishore bjv <jb619d@att.com>
2017-03-05 02:23:40 +0530

    Updating randomize_meals action to allocate meal count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,21 @@ class HomeController < ApplicationController
   end
   def allocate_meals
   end
+# This action generates random count of meals & Clears the allocated meals to User
+# It will not modify user preferences
   def randomize_meals
+        Meal_Box.all.each do |box|
+         box.total_count=rand(1..2)
+         box.available=box.total
+         box.save
+        end
+       
+	User.all.each do |user|
+      	  user.meal_of_day = nil
+          user.save
+        end
+     
+     redirect_to :back
   end
   def reset
   end
