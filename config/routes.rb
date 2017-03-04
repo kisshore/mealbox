c70668e7e8af65699690b81ad8ca2c40b977abec
kishore bjv <jb619d@att.com>
2017-03-05 02:47:49 +0530

    Updating routes for allocate_meals action

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
   get '/randomize_meals' => 'home#randomize_meals'
+  get '/allocate_meals'  => 'home#allocate_meals'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
