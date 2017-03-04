66123206143ea0b9899c2860d3ec14e7e8e29672
kishore bjv <jb619d@att.com>
2017-03-05 02:46:39 +0530

    Writing allocate_meals action to allocate meals to users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,24 @@
 class HomeController < ApplicationController
   def index
   end
+
   def allocate_meals
+       User.all.each do |user|
+       if user.meal_of_the_day.nil?
+          if meal=Meal_Box.where(category: user.likes).where("available > ?", 0).first
+             user.meal_of_the_day = meal.name
+             meal.available = meal.available-1
+             meal.save
+          elsif meal=Meal_Box.where(category: user.no_pref).where("available > ?", 0).first
+             user.meal_of_the_day = meal.name
+             meal.available = meal.available-1
+             meal.save
+           end
+       end
+      user.save
+     end
+     redirect_to :back
+
   end
 # This action generates random count of meals & Clears the allocated meals to User
 # It will not modify user preferences
